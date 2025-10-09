54a19e33982f9de3155ae3f020ab2125fa30959e
yosemm <140467686+yosemm@users.noreply.github.com>
2025-10-08 22:14:37 -0600

    Add missing screen

---
+++ app/src/main/java/com/example/lab9/LikeCounterScreen.kt
@@ -0,0 +1,39 @@
+package com.example.lab9
+
+// Esta pantalla es para evidenciar que el estado con remember no sobrevive cambios de configuración.
+
+import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LikeCounterScreen() {
+    var likeCount by remember { mutableStateOf(0) }
+
+    Scaffold { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Contador de Me Gusta", style = MaterialTheme.typography.headlineMedium)
+            Spacer(Modifier.height(16.dp))
+            Text("$likeCount", style = MaterialTheme.typography.displayLarge)
+            Spacer(Modifier.height(24.dp))
+            Button(onClick = { likeCount++ }) { Text("¡Me Gusta!") }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun ScreenPreview(){
+    LikeCounterScreen()
+}
